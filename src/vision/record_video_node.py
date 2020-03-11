09f1778ec6e2deba671b00596c2a785cefc25c73
naterenegar <naterenegar@gmail.com>
2020-03-10 20:26:44 -0400

    fixed up record_video_node.py

--- src/vision/record_video_node.py
+++ src/vision/record_video_node.py
@@ -1,16 +1,48 @@
+#!/usr/bin/python
+
 import cv2
 import rospy
-from sensor_msgs import Image
+from sensor_msgs.msg import Image
 import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+import argparse
 
-def callback(data):
-	img = np.reshape(data.data, (data.height,data.width))
-	cv2.imshow("image", img)
+class image_converter:
 
-def listener():
+	def __init__(self,args):
+		self.image_sub = rospy.Subscriber("camera/image_color",Image,self.callback)
+		self.bridge = CvBridge()
+	
+	def callback(self,data):
+		try:
+			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+			cv_image = cv2.resize(cv_image, (600, 800))
+			cv2.imshow("image", cv_image)
+			cv2.waitKey(1)
+		except CvBridgeError as e:
+			print(e)	
+
+def main():
+
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-d", "--display", type=bool, default=True,
+		help="Toggles display of video")
+	ap.add_argument("-o", "--output", default=None,
+		help="path to output video file")
+	ap.add_argument("-f", "--fps", type=int, default=30,
+		help="FPS of output video")
+
+	args = vars(ap.parse_args())
+
+
+	ic = image_converter(args)
 	rospy.init_node("listener", anonymous=True)
-	rospy.Subscriber("camera/image_raw", Image, callback)
-	rospy.spin()
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print("Shutting down")
+	cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
-	listener()
+	main()
