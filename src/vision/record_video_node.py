af6fc512a84c9c678f6eeb0ed24a24422c59ec7f
naterenegar <naterenegar@gmail.com>
2020-03-12 19:09:42 -0400

    recording changes

--- src/vision/record_video_node.py
+++ src/vision/record_video_node.py
@@ -9,28 +9,51 @@ import argparse
 
 class image_converter:
 
+
+
 	def __init__(self,args):
 		self.image_sub = rospy.Subscriber("camera/image_color",Image,self.callback)
 		self.bridge = CvBridge()
-	
+		self.actions = []
+		if args["output"]:
+			self.video_writer = cv2.VideoWriter(args["output"],
+							    cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
+							    args["fps"],
+							    (args["width"], args["height"]))
+			self.actions.append(lambda frame: self.video_writer.write(frame))
+
+		if args["display"]:
+			def display_delay(frame):
+				cv2.imshow("image")
+				cv2.waitKey(1)
+			self.actions.append(display_delay)
+
+		# create lambdas for configurable callback behavior (all must take one input, the frame)
+
+
 	def callback(self,data):
 		try:
-			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-			cv_image = cv2.resize(cv_image, (600, 800))
-			cv2.imshow("image", cv_image)
-			cv2.waitKey(1)
+			frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
+			for action in self.actions:
+				action(frame)
 		except CvBridgeError as e:
 			print(e)	
+
+
 
 def main():
 
 	ap = argparse.ArgumentParser()
 	ap.add_argument("-d", "--display", type=bool, default=True,
-		help="Toggles display of video")
+			help="Toggles display of video")
 	ap.add_argument("-o", "--output", default=None,
-		help="path to output video file")
+			help="path to output video file")
 	ap.add_argument("-f", "--fps", type=int, default=30,
-		help="FPS of output video")
+			help="FPS of output video")
+	ap.add_argument("-w", "--width", type=int, default=1280,
+			help="Width in pixels of camera stream")
+	ap.add_argument("-l", "--height", type=int, default=1024,
+			help="Height in pixels of camera stream")
 
 	args = vars(ap.parse_args())
 
